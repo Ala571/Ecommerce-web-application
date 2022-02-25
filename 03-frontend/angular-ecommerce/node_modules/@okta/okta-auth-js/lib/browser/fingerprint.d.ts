1693631e857363d03208abac254f158c8288a3d9
Ala571 <ala.baklouti@esprit.tn>
2022-02-25 19:07:57 +0100

    first commit

---
+++ 03-frontend/angular-ecommerce/node_modules/@okta/okta-auth-js/lib/browser/fingerprint.d.ts
@@ -0,0 +1,3 @@
+import { OktaAuth } from '../types';
+import { FingerprintOptions } from '../types';
+export default function fingerprint(sdk: OktaAuth, options: FingerprintOptions): Promise<unknown>;
---
+++ 03-frontend/angular-ecommerce/node_modules/@okta/okta-auth-js/lib/types/AuthState.d.ts
@@ -0,0 +1,14 @@
+import { AccessToken, IDToken, RefreshToken, Token } from './Token';
+export interface AuthState {
+    accessToken?: AccessToken;
+    idToken?: IDToken;
+    refreshToken?: RefreshToken;
+    isAuthenticated?: boolean;
+    isPending?: boolean;
+    error?: Error;
+}
+export interface AuthStateLogOptions {
+    event?: string;
+    key?: string;
+    token?: Token;
+}
